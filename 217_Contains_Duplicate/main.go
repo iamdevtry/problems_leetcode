6281abe88a44ef31d8d55bf1a87647280209ed9e
iamdevtry <tranbinhphongcmg@gmail.com>
2023-04-16 17:01:25 +0700

    solved 217

---
+++ 217_Contains_Duplicate/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func containsDuplicate(nums []int) bool {
+	elements := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		elements[nums[i]]++
+	}
+
+	for _, x := range elements {
+		if x >= 2 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	// arr := []int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}
+	// arr := []int{1, 2, 3, 1}
+	arr := []int{1, 2, 3, 4}
+	fmt.Println(containsDuplicate(arr))
+}
