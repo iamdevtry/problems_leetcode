7aecafdf4123219b2e0bad92ff6a685eaa068f22
iamdevtry <tranbinhphongcmg@gmail.com>
2023-04-16 17:15:53 +0700

    optimized problem 217

--- 217_Contains_Duplicate/main.go
+++ 217_Contains_Duplicate/main.go
@@ -7,10 +7,7 @@ func containsDuplicate(nums []int) bool {
 
 	for i := 0; i < len(nums); i++ {
 		elements[nums[i]]++
-	}
-
-	for _, x := range elements {
-		if x >= 2 {
+		if elements[nums[i]] > 1 {
 			return true
 		}
 	}
@@ -20,7 +17,7 @@ func containsDuplicate(nums []int) bool {
 
 func main() {
 	// arr := []int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}
-	// arr := []int{1, 2, 3, 1}
-	arr := []int{1, 2, 3, 4}
+	arr := []int{1, 2, 3, 1}
+	// arr := []int{1, 2, 3, 4}
 	fmt.Println(containsDuplicate(arr))
 }
